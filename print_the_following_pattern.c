1d4b0e97f5b5183e67f060f616dbe3173b31522b
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-02-01 19:09:56 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("%c ",i+64);         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,14 +1,16 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<stdio.h>
 int main()
 {
-    int i, j, n ;
+    int i,j,n;
     scanf("%d",&n);
-    for (i = n; i >= 1; i--) {
-        for (j = 1; j <= i; j++) {
-            cout << (char)('A' - 1 + i) << " ";
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("%c ",i+64);
         }
-        cout << endl;
+        printf("
+");
     }
     return 0;
 }
