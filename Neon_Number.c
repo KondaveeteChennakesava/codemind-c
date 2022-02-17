3c3cc3631b9121532c7b6696b6b3c665978ae3d6
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-02-17 20:17:34 +0530

    #include<stdio.h> int main() {     int n,a,k,s=0;     scanf("%d",&n);     k=n*n;     while(k>0)     {         a=k%10;         s=s+a;         k=k/10;     }     if(s==n)     {         printf("Neon Number");     }     else     {         printf("Not Neon Number");     } }

--- Neon_Number.c
+++ Neon_Number.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
 int main()
-{ 
-    int n,s=0,q,p,rem;
+{
+    int n,a,k,s=0;
     scanf("%d",&n);
-    p=n*n;
-    q=p;
-    while(q>0)
-	{
-		rem=q%10;
-		s=s+rem;
-		q=q/10;
-	}
-	if(s==n)
-	{
-		printf("Neon Number");
-	}
-	else
-	{
-		printf("Not Neon Number");
-	}
+    k=n*n;
+    while(k>0)
+    {
+        a=k%10;
+        s=s+a;
+        k=k/10;
+    }
+    if(s==n)
+    {
+        printf("Neon Number");
+    }
+    else
+    {
+        printf("Not Neon Number");
+    }
 }
