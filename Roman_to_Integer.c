f4a8900a84da8b9b5a3e307850b53a8761213ede
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-06-27 21:59:45 +0530

    #include<stdio.h> #include<string.h>
    int digit(char);
     
    int main(){
     
        char roman_Number[1000];
        int i=0;
        long int number =0;
     
        
        scanf("%s",roman_Number);
     
        while(roman_Number[i]){
     
             if(digit(roman_Number[i]) < 0){
                 printf("Invalid roman digit : %c",roman_Number[i]);
                 return 0;
             }
     
             if((strlen(roman_Number) -i) > 2){
                 if(digit(roman_Number[i]) < digit(roman_Number[i+2])){
                     printf("Invalid roman number");
                     return 0;
                 }
             }
     
             if(digit(roman_Number[i]) >= digit(roman_Number[i+1]))
                 number = number + digit(roman_Number[i]);
             else{
                 number = number + (digit(roman_Number[i+1]) - digit(roman_Number[i]));
                 i++;
             }
             i++;
        }
     
        printf("%ld",number);
     
        return 0;
     
    }
     
    int digit(char c){
     
        int value=0;
     
        switch(c){
             case 'I': value = 1; break;
             case 'V': value = 5; break;
             case 'X': value = 10; break;
             case 'L': value = 50; break;
             case 'C': value = 100; break;
             case 'D': value = 500; break;
             case 'M': value = 1000; break;
             case NULL: value = 0; break;
             default: value = -1; 
        }
     
        return value;
    }

--- Roman_to_Integer.c
+++ Roman_to_Integer.c
@@ -1,71 +1,61 @@
-#include <stdio.h>
-#include <string.h>
-
-int convert(char);  //function prototype of conversion function
-
-int main ()
-{
-   int i = 0, num = 0;
-   char romanNumber[100]; //array for storing roman number
-
-
-   scanf("%s", romanNumber);
-
-   while(romanNumber[ i ])
-   {
-      //if condition for checking validity of roman number
-      if(convert(romanNumber[ i ]) < 0 )
-      {
-         printf("
-Invalid Roman Number.
-");
-         return 0;
-      }
-
-      //if condition for checking validity of roman number
-      if((strlen(romanNumber) - i ) > 2)
-      {
-         if(convert(romanNumber[ i ]) < convert(romanNumber[i + 2]))
-         {
-            printf("
-Invalid Roman Number.
-");
-            return 0;
+#include<stdio.h>
+#include<string.h>
+ 
+int digit(char);
+ 
+int main(){
+ 
+    char roman_Number[1000];
+    int i=0;
+    long int number =0;
+ 
+    
+    scanf("%s",roman_Number);
+ 
+    while(roman_Number[i]){
+ 
+         if(digit(roman_Number[i]) < 0){
+             printf("Invalid roman digit : %c",roman_Number[i]);
+             return 0;
          }
-      }
-
-      //if condition for converting roman number into decimal number
-      if(convert(romanNumber[ i ]) >= convert(romanNumber[i + 1])) //true is first roman number is greater or equal to second
-         num = num + convert(romanNumber[ i ]);
-      else
-      {
-         num = num + (convert(romanNumber[i + 1]) - convert(romanNumber[ i ]));
+ 
+         if((strlen(roman_Number) -i) > 2){
+             if(digit(roman_Number[i]) < digit(roman_Number[i+2])){
+                 printf("Invalid roman number");
+                 return 0;
+             }
+         }
+ 
+         if(digit(roman_Number[i]) >= digit(roman_Number[i+1]))
+             number = number + digit(roman_Number[i]);
+         else{
+             number = number + (digit(roman_Number[i+1]) - digit(roman_Number[i]));
+             i++;
+         }
          i++;
-      }
-      i++;
-   }
-
-   //displaying converted number
-   printf("%d", num);
-   return 0;
+    }
+ 
+    printf("%ld",number);
+ 
+    return 0;
+ 
 }
-
-//converting roman number into equivalent decimal value
-int convert(char ch)
-{
-   int value = 0;
-
-   switch(ch)
-   {
-      case 'I': value = 1; break;
-      case 'V': value = 5; break;
-      case 'X': value = 10; break;
-      case 'L': value = 50; break;
-      case 'C': value = 100; break;
-      case 'D': value = 500; break;
-      case 'M': value = 1000; break;
-      case NULL: value = 0; break;
-      default: value = -1;
-   }
-   return value;
+ 
+int digit(char c){
+ 
+    int value=0;
+ 
+    switch(c){
+         case 'I': value = 1; break;
+         case 'V': value = 5; break;
+         case 'X': value = 10; break;
+         case 'L': value = 50; break;
+         case 'C': value = 100; break;
+         case 'D': value = 500; break;
+         case 'M': value = 1000; break;
+         case NULL: value = 0; break;
+         default: value = -1; 
+    }
+ 
+    return value;
 }
