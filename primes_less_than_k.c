14816e7e2369e21b35083d391f1ca085d9dbdd7d
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-07-02 21:43:36 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,k,j,c=0,m=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         c=0;         for(j=1;j<=arr[i];j++)         {             if(arr[i]%j==0)             {                 c++;             }         }         if(c==2)         {         if(arr[i]<=k)         {             m++;         }         }     }     printf("%d",m); }

---
+++ primes_less_than_k.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,k,j,c=0,m=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=1;j<=arr[i];j++)
+        {
+            if(arr[i]%j==0)
+            {
+                c++;
+            }
+        }
+        if(c==2)
+        {
+        if(arr[i]<=k)
+        {
+            m++;
+        }
+        }
+    }
+    printf("%d",m);
+}
