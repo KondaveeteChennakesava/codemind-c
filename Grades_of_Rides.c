5272f197f08540c38005d566cc9dffa28d7c2a32
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-07-29 21:06:54 +0530

    #include<stdio.h> int main() {     int huri,spin,speed;     scanf("%d%d%d",&huri,&spin,&speed);     if(huri>50&&spin>60&&speed>100)     {         printf("10");     }     else if(huri>50&&spin>60&&speed<=100)     {         printf("9");     }     else if(huri<=50&&spin>60&&speed>100)     {         printf("8");     }     else if(huri>50&&spin<=60&&speed>100)     {         printf("7");     }     else if(huri>50||spin>60||speed>100)     {         printf("6");     }     else     {         printf("5");     }
    }

--- Grades_of_Rides.c
+++ Grades_of_Rides.c
@@ -1,19 +1,31 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
-    if (a>50&&b>60&&c>100)
-    printf("10");
-    else if(a>50&&b>60)
-    printf("9");
-    else if(a>50&&c>100)
-    printf("7");
-    else if(b>60&&c>100)
-    printf("8");
-    else if(a>50 || b>60 || c>100)
-    printf("6");
+    int huri,spin,speed;
+    scanf("%d%d%d",&huri,&spin,&speed);
+    if(huri>50&&spin>60&&speed>100)
+    {
+        printf("10");
+    }
+    else if(huri>50&&spin>60&&speed<=100)
+    {
+        printf("9");
+    }
+    else if(huri<=50&&spin>60&&speed>100)
+    {
+        printf("8");
+    }
+    else if(huri>50&&spin<=60&&speed>100)
+    {
+        printf("7");
+    }
+    else if(huri>50||spin>60||speed>100)
+    {
+        printf("6");
+    }
     else
-    printf("5");
-    return 0;
+    {
+        printf("5");
+    }
+    
 }
