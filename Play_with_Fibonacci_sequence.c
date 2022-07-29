ea4f8402fe90869c821ba0f039c1809ef2582e68
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-07-29 21:39:12 +0530

    #include<stdio.h> int main() {     int n,a=0,b=1,c,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         printf("%d ",a);         c=a+b;         a=b;         b=c;     } }

--- Play_with_Fibonacci_sequence.c
+++ Play_with_Fibonacci_sequence.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,b=0,d=1,c;
+    int n,a=0,b=1,c,i;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        printf("%d ",b);
-        c=b+d;
-        b=d;
-        d=c;
+        printf("%d ",a);
+        c=a+b;
+        a=b;
+        b=c;
     }
 }
