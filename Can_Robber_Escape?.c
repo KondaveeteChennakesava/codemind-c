c49d82374ca215f74d19032ee6d5e4701a5b421c
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-07-29 21:02:35 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i,c=0;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]>=n)         {             c=1;             break;         }     }     if(c==1)     {         printf("NO");     }     else     {         printf("YES");     } }

--- Can_Robber_Escape?.c
+++ Can_Robber_Escape?.c
@@ -1,18 +1,27 @@
-#include<stdio.h> 
+#include<stdio.h>
 int main()
 {
-    int n,x[20],odd=0,i;
+    int n;
     scanf("%d",&n);
-    for(i=0;i<n;i++) 
-    scanf("%d",&x[i]);
-    for(i=0;i<n;i++) 
-    { if(x[i]%2!=0) 
-    odd++;
-   }
-   if(odd<=2)
-   printf("YES");
-   else
- printf("NO");
- return 0; 
-    
+    int arr[n],i,c=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=n)
+        {
+            c=1;
+            break;
+        }
+    }
+    if(c==1)
+    {
+        printf("NO");
+    }
+    else
+    {
+        printf("YES");
+    }
 }
