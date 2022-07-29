cc075d07002f76cdb8cdd35c20aacc457136db9c
Chennak2248 <96309319+Chennak2248@users.noreply.github.com>
2022-07-29 23:15:13 +0530

    #include<stdio.h>
        int main()
        {
            int n;
            scanf("%d",&n);
            int a[n],i,j,c=0;
            for(i=0;i<n;i++)
            {
                scanf("%d",&a[i]);
            }
            for(i=0;i<n;i++)
            {
                c=0;
                for(j=0;j<n;j++)
                {
                    if(i!=j)
                    {
                        if(a[i]>a[j])
                        {
                            c++;
                        }
                    }
                }
                printf("%d ",c);
            }
        }

--- How_Many_Numbers_Are_Smaller_Than_the_Current_Number.c
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.c
@@ -1,23 +1,27 @@
 #include<stdio.h>
-int main()
-{
-    int n,arr[100],i,j,c=0;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
+ 
+    int main()
     {
-        scanf("%d",&arr[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        c=0;
-        for(j=0;j<n;j++)
+        int n;
+        scanf("%d",&n);
+        int a[n],i,j,c=0;
+        for(i=0;i<n;i++)
         {
-            if(arr[i]>arr[j])
-            {
-                c++;
-            }
+            scanf("%d",&a[i]);
         }
-        printf("%d ",c);
+        for(i=0;i<n;i++)
+        {
+            c=0;
+            for(j=0;j<n;j++)
+            {
+                if(i!=j)
+                {
+                    if(a[i]>a[j])
+                    {
+                        c++;
+                    }
+                }
+            }
+            printf("%d ",c);
+        }
     }
-    
-}
